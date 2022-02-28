e95e46f8d7726da2638d85d1170c1c4a61585cd4
James Hwang <siro257@gmail.com>
2022-02-28 11:07:27 -0800

    homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
 import Layout from "./layout/Layout";
+import Header from "./layout/Header";
+import { UserProvider } from "../context/UserConext";
+import { Route, Switch } from "react-router";
+import Home from "./pages/Home";
+import ErrorBoundary from "./base/ErrorBoundary";
+
 
 function App() {
   return (
     <>
-      <Layout></Layout>
+      <UserProvider>
+        <Layout>
+          <Header />
+          <Switch>
+            <Route path="/sales"></Route>
+            <Route path="/reviews"></Route>
+            <Route path="/">
+              <ErrorBoundary>
+                <Home />
+              </ErrorBoundary>
+            </Route>
+          </Switch>
+        </Layout>
+      </UserProvider>
+
     </>
   );
 }
